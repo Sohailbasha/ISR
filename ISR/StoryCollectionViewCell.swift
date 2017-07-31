688e1515ddb0eca2069600117962e3d74bc32983
Sohailbasha <shlbsh94@gmail.com>
2017-07-31 16:09:40 -0400

    added new view controller and collection view. Subclassed both UIobjects.

---
+++ ISR/StoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StoryCollectionViewCell.swift
+//  ISR
+//
+//  Created by Ilias Basha on 7/31/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class StoryCollectionViewCell: UICollectionViewCell {
+    
+}
