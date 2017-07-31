e94f31d2dcaafe7e2cbaf38154bb1a733c3757f8
Sohailbasha <shlbsh94@gmail.com>
2017-07-31 12:26:25 -0400

    added source model object

---
+++ ISR/Source.swift
@@ -0,0 +1,17 @@
+//
+//  Source.swift
+//  ISR
+//
+//  Created by Ilias Basha on 7/31/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import Foundation
+
+struct Source {
+    
+    let id: String
+    let name: String
+    let description: String
+    
+}
--- MainViewController.swift
+++ MainViewController.swift
@@ -19,6 +19,8 @@ class MainViewController: UIViewController {
 
     
     
+    
+    
 
     /*
     // MARK: - Navigation
