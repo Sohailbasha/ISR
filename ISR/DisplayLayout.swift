a6d2822a92953dabbd2350874e8001299619d535
Sohailbasha <shlbsh94@gmail.com>
2017-07-31 21:15:31 -0400

    added in a UICollectionViewLayout to achieve a desired layout effect for my collection view cells.

---
+++ ISR/DisplayLayout.swift
@@ -0,0 +1,13 @@
+//
+//  DisplayLayout.swift
+//  ISR
+//
+//  Created by Ilias Basha on 7/31/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class DisplayLayout: UICollectionViewLayout {
+
+}
